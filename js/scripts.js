e4fac4ea6bf103b30fff46683ae33ff72608ff03
William Kulha <kulha.william@gmail.com>
2018-09-29 11:49:43 -0400

    Add javascript with a few custom functions to parse user input, run the input through a for loop while pushing each iteration to an array, then joining the array and printing it to #keepingCount

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
+let makeNumber = function(input) {
+  let stringNumber = $(input).val();
+  return parseInt(stringNumber);
+}
 
-
+let makeItPrint = function(input) {
+  let toPrint = input.join(", ");
+  $('#keepingCount').append(toPrint)
+}
 
 $(document).ready(function() {
   $('#userInput').submit(function(event) {
     event.preventDefault();
-    let countTo = $('#countTo').val();
-    let countBy = $('#countBy').val();
-    console.log(countTo, countBy);
-
-
+    let countTo = makeNumber($('#countTo'));
+    let countBy = makeNumber($('#countBy'));
+    let trackCount = []
+    for (var index = countBy; index <= countTo; index += countBy) {
+      trackCount.push(index);
+    }
+    makeItPrint(trackCount);
   });
 });
