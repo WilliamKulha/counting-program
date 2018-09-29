84a6b7e87b8801c27e46cff15028f4c3e2e98939
William Kulha <kulha.william@gmail.com>
2018-09-29 14:38:49 -0400

    Refactor the code so that the long if else loop is part of an earlier declared function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+let error = "Please make sure that the number you're counting to is larger than the number you're counting by."
+
 let makeNumber = function(input) {
   let stringNumber = $(input).val();
   return parseInt(stringNumber);
@@ -8,27 +10,35 @@ let makeItPrint = function(input) {
   $('#keepingCount').html(toPrint)
 }
 
-let error = "Please make sure that the number you're counting to is larger than the number you're counting by."
+let compareNumbers = function(countTo, countBy) {
+  let trackCount = []
+  if (countTo >= 1 && countBy > countTo) {
+    $('#errorMessage').empty();
+    $('#errorMessage').append(error)
+  }
+  else if (countTo <=0 && countBy <=0) {
+    $('#errorMessage').empty();
+    for (var index = 0; index < countTo; index -= countBy) {
+      trackCount.push(index);
+    }
+    makeItPrint(trackCount);
+  }
+  else {
+    $('#errorMessage').empty();
+    for (var index = countBy; index <= countTo; index += countBy) {
+      trackCount.push(index);
+    }
+    makeItPrint(trackCount);
+  }
+}
 
 $(document).ready(function() {
   $('#userInput').submit(function(event) {
     event.preventDefault();
     let countTo = makeNumber($('#countTo'));
     let countBy = makeNumber($('#countBy'));
-    let trackCount = []
-    if (countBy >= countTo) {
-      $('#errorMessage').empty();
-      $('#errorMessage').append(error)
-    }
-    else if (countTo <=0 && countBy <=0) {
-      
-    }
-    else {
-      $('#errorMessage').empty();
-      for (var index = countBy; index <= countTo; index += countBy) {
-        trackCount.push(index);
-      }
-      makeItPrint(trackCount);
-    }
+    compareNumbers(countTo, countBy);
+
+
   });
 });
