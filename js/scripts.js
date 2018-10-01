d7a92b1bc7373b06f3fe082746c764ca1ac56ef2
William Kulha <kulha.william@gmail.com>
2018-10-01 09:05:32 -0400

    Refactor code and add logic that will allow users to count with negative numbers & add an error message to be displayed if the user tries to use one negative and one positive number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,41 @@
-let error = "Please make sure that the number you're counting to is larger than the number you're counting by."
+//Back-end logic
 
+//Define error messages.
+let error = "Please make sure that the number you're counting to is larger than the number you're counting by."
+let posNegError = "Please don't use one negative number and one positive number."
+
+//Get user input.
 let makeNumber = function(input) {
   let stringNumber = $(input).val();
   return parseInt(stringNumber);
 }
-
+//Define a function to join an array with a specific character and then push it to the html tag.
 let makeItPrint = function(input) {
   let toPrint = input.join(", ");
   $('#keepingCount').html(toPrint)
 }
-
+//Figure out if user input is valid and then use for loops to count.
 let compareNumbers = function(countTo, countBy) {
   let trackCount = []
+  //Display error if user is trying to count by a number larger than the one they're counting to.
   if (countTo >= 1 && countBy > countTo) {
     $('#errorMessage').empty();
     $('#errorMessage').append(error)
   }
-  else if (countTo <=0 && countBy <=0) {
+  //Display error if user is trying to use one negative number and one positive number.
+  else if (countTo < 0 && countBy > 0 || countTo > 0 && countBy < 0) {
     $('#errorMessage').empty();
-    for (var index = 0; index < countTo; index -= countBy) {
+    $('#errorMessage').append(posNegError)
+  }
+  //For loop for counting negative numbers.
+  else if (countTo < 0 && countBy < 0) {
+    $('#errorMessage').empty();
+    for (var index = countBy; index >= countTo; index += countBy) {
       trackCount.push(index);
     }
     makeItPrint(trackCount);
   }
+  //For loop for counting positive numbers.
   else {
     $('#errorMessage').empty();
     for (var index = countBy; index <= countTo; index += countBy) {
@@ -32,13 +45,12 @@ let compareNumbers = function(countTo, countBy) {
   }
 }
 
+//Front-end logic.
 $(document).ready(function() {
   $('#userInput').submit(function(event) {
     event.preventDefault();
     let countTo = makeNumber($('#countTo'));
     let countBy = makeNumber($('#countBy'));
     compareNumbers(countTo, countBy);
-
-
   });
 });
